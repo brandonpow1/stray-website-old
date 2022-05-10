6118706971650bf81fcbf09141ce8ecf13a5fe77
brandonpow1 <pow.brandon@gmail.com>
2022-05-10 15:45:38 +0800

    init

---
+++ src/nav.js
@@ -0,0 +1,15 @@
+fetch('nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+
+    const btn = document.querySelector("button.mobile-menu-button");
+    const menu = document.querySelector(".mobile-menu");
+
+    btn.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
+})
